42fd6404d055a4386911049006469dda5d91cede
Ivan Deyev <deyevivan777@mail.ru>
2026-05-01 01:36:09 +0500

    deploy preparing

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -9,11 +9,21 @@ from alembic import context
 
 from app.database import Base
 from app import models
+import os
 
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set")
+
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
+
+
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- readme.md
+++ readme.md
@@ -4,3 +4,9 @@ API:
 http://SERVER_IP:8000
 Swagger:
 http://SERVER_IP:8000/docs
+
+Миграции:
+
+alembic upgrade head
+->
+docker compose up -d
